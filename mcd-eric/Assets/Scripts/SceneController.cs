8c3398525c58c4014dceb6c6277a5b3e2221bf38
Eric Salczynski <eric@wehaventthetime.com>
2013-05-25 13:43:55 -0400

    Retry?

--- mcd-eric/Assets/Scripts/SceneController.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-// Put any scene-related static vars in this class.
-// DOES NOT have to be attached as a component on a GameObject.
-public class SceneController : MonoBehaviour {
-    public const float GRAVITY = -600f;
-}
